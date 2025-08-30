c45be370f01eb2e6e7309867a5adb1f49ddad68e
Jeffrey Cordero <94331560+jeffrey-cordero@users.noreply.github.com>
2025-08-30 10:01:35 -0400

    Add Landing Layout Wrapper for Authenticated Clients (#18)
    * Refactor route structure to include nested layout for authentication routes in routes.tsx
    
    * Linting
    
    * Add EOF

--- client/app/routes.tsx
+++ client/app/routes.tsx
@@ -6,9 +6,11 @@ import { layout, route, type RouteConfig } from "@react-router/dev/routes";
  */
 export default [
    layout("routes/authentication/layout.tsx", [
-      route("/", "routes/page.tsx"),
-      route("register", "routes/authentication/register.tsx"),
-      route("login", "routes/authentication/login.tsx"),
+      layout("routes/layout.tsx", [
+         route("/", "routes/page.tsx"),
+         route("register", "routes/authentication/register.tsx"),
+         route("login", "routes/authentication/login.tsx")
+      ]),
       layout("routes/dashboard/layout.tsx", [
          route("dashboard", "routes/dashboard/page.tsx"),
          route("dashboard/accounts", "routes/dashboard/accounts/page.tsx"),
