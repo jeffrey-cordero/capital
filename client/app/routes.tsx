1549479dfa851fe81e1bf260a0f6c554602a9f6c
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-08-30 09:59:32 -0400

    Linting

--- client/app/routes.tsx
+++ client/app/routes.tsx
@@ -9,7 +9,7 @@ export default [
       layout("routes/layout.tsx", [
          route("/", "routes/page.tsx"),
          route("register", "routes/authentication/register.tsx"),
-         route("login", "routes/authentication/login.tsx"),
+         route("login", "routes/authentication/login.tsx")
       ]),
       layout("routes/dashboard/layout.tsx", [
          route("dashboard", "routes/dashboard/page.tsx"),
