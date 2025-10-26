8c4bd12b576da7abfdfe43fd4f9f4444a8c1c003
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-10-26 19:31:15 -0400

    Remove logs

--- server/lib/database.ts
+++ server/lib/database.ts
@@ -8,11 +8,6 @@ import { logger } from "@/lib/logger";
 /**
  * Connection pool for database connections
  */
-console.log("DB_HOST", process.env.DB_HOST);
-console.log("DB_USER", process.env.DB_USER);
-console.log("DB_PASSWORD", process.env.DB_PASSWORD);
-console.log("DB_NAME", process.env.DB_NAME);
-console.log("DB_PORT", process.env.DB_PORT);
 const pool = new Pool({
    host: process.env.DB_HOST || "postgres",
    user: process.env.DB_USER,
