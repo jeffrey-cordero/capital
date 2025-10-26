c1112d6453d2f9e49a5be036dc2dde853741d28c
Jeffrey Cordero <JeffreyCorderoNY@gmail.com>
2025-10-26 19:30:27 -0400

    Revert DB SSL param

--- server/lib/database.ts
+++ server/lib/database.ts
@@ -8,6 +8,11 @@ import { logger } from "@/lib/logger";
 /**
  * Connection pool for database connections
  */
+console.log("DB_HOST", process.env.DB_HOST);
+console.log("DB_USER", process.env.DB_USER);
+console.log("DB_PASSWORD", process.env.DB_PASSWORD);
+console.log("DB_NAME", process.env.DB_NAME);
+console.log("DB_PORT", process.env.DB_PORT);
 const pool = new Pool({
    host: process.env.DB_HOST || "postgres",
    user: process.env.DB_USER,
@@ -15,7 +20,7 @@ const pool = new Pool({
    database: process.env.DB_NAME,
    port: Number(process.env.DB_PORT) || 5432,
    max: 50,
-   ssl: process.env.DB_HOST !== "postgres" ? { rejectUnauthorized: false } : false
+   ssl: { rejectUnauthorized: false }
 });
 
 /**
